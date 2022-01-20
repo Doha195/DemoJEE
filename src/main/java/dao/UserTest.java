90eabca84bac984616b240bc36dbf585b97a83c8
doha <dohaferiadi@gmail.com>
2022-01-20 10:15:13 +0100

    Test unitaire

--- src/main/java/dao/UserTest.java
+++ src/main/java/dao/UserTest.java
@@ -1,37 +1,46 @@
 package dao;
 
-import static org.junit.Assert.*;
-import Entities.User;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.sql.SQLException;
 
 import org.junit.Test;
+
+import Entities.User;
 
 public class UserTest {
 
 	@Test
 	public void testsaveUser() {
 		UserDao userdao = new UserDao();
-		//User user = new User( "moroc", "dojjha@gmail.com", "hgjhg");
-		//userdao.saveUser(user);
+		User user = new User( "moroco", "dojjha@gmail.com", "hgjhg");
+	    //userdao.saveUser(user);
+
+		 //assertEquals("hgjhg", user.getName());
 		
 	}
 	
 	@Test 
 	public void testupdateuser() {
 		UserDao userdao = new UserDao();
-		//User user = new User(1, "moroc", "dohaferiadi@gmail.com", "doha");
+		User user = new User(21, "moroc", "dohaferiadi@gmail.com", "doha");
 		//userdao.updateUser(user);
+		//assertEquals("doha", user.getName());
 	}
 	
 	@Test 
 	public void testgetuser() {
 		UserDao userdao = new UserDao();
-		//System.out.println(userdao.getUser(1));
+		userdao.getUser(1);
+		assertEquals("doha",userdao.getUser(1).getName());
 	}
 	
 	@Test 
 	public void testgetAllUser() {
 		UserDao userdao = new UserDao();
-		System.out.println(userdao.getAllUser());
+		assertFalse(userdao.getAllUser().isEmpty());
 	}
+	
 
 }
