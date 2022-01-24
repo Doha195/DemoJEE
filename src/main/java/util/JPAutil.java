e4fa1277cae1688ef02ee7a51b45b50c1e45646e
doha <dohaferiadi@gmail.com>
2022-01-24 11:18:56 +0100

    adding JPAutil

---
+++ src/main/java/util/JPAutil.java
@@ -0,0 +1,16 @@
+package util;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JPAutil {
+    public static EntityManagerFactory entityManagerFactory(){
+        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("default");
+        return entityManagerFactory;
+    }
+
+
+}
